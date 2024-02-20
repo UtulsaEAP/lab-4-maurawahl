15d38290db11f639bc6cb824fe8bf8b0e775f474
maurawahl <mjw4048@utulsa.edu>
2024-02-20 12:48:24 -0600

    problem 4 done

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -1,12 +1,16 @@
 """
 Complete the following python code to reverse the string entered by the user.
 
-Name: 
-Lab Time:
+Name: Maura Wahl
+Lab Time: Thursdays at 2 pm
 """
 
 def reverse_string():
     # YOUR CODE HERE
+    word = str(input())
+    while len(word)>=1 and word != "done" and word != "Done" and word != "d":
+        print(word[::-1])
+        word = str(input())
     pass
 if __name__ == "__main__":
     reverse_string()
