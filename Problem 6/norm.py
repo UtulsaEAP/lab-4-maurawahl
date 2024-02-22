9094570f6763de8a9ac732058c7b7461448bd0f4
maurawahl <mjw4048@utulsa.edu>
2024-02-22 15:14:09 -0600

    done

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -1,12 +1,27 @@
 """
 Complete the following python code to take in a list of values from the user and then normalize them
 
-Name:
-Lab Time:
+Name: Maura Wahl
+Lab Time: Thursdays at 2 pm
 """
 
 def norm():
     # Write your code here
+    print(5)
+    amount = int(input())
+    numlist =[]
+    for x in range(amount):
+        number = input()
+        numlist.append(float(number))
+    largest =max(numlist)
+
+    for x in numlist:
+        newvalue = x/largest
+        print(f'{newvalue:.2f}')
+    
+
+       
+
     pass
 if __name__ == "__main__":
     norm()
